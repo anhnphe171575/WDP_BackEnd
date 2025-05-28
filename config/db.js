cfbf5fd5a617e834de8b2f458a4516b7560c72eb
QuanPM <anhnphe171575@fpt.edu.vn>
2025-05-29 01:03:59 +0700

    jwt

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 // URL kết nối (thường là mongodb://localhost:27017 hoặc URI Atlas)
-const uri = 'mongodb+srv://phuanhpro11:123456@wdp.5nczhuu.mongodb.net/';
+const uri = 'mongodb+srv://phuanhpro11:123@wdp.5nczhuu.mongodb.net/?retryWrites=true&w=majority&appName=WDP';
 
 // Tên database
 const dbName = 'petnest';
