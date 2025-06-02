a6b2be03b28471442b0a62f72355bdd0c289950c
QuanPM <anhnphe171575@fpt.edu.vn>
2025-06-03 01:04:11 +0700

    fix

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/UserModel');
+
 const banner = require('../models/bannerModel');
 /**
  * @swagger
