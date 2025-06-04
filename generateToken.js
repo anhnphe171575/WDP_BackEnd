a251885635e84c6e50d1ffaa02c89e4304f62878
quanpmhe171342 <quanpmhe171342@fpt.edu.vn>
2025-06-04 16:43:17 +0700

    fix confict

--- generateToken.js
+++ generateToken.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const payload = {
-  id: "6836d927d6b8bb6da4e00228", // ID giả lập user, ông thay bằng ID thật nếu cần
-  name: "Nguyen Van A",
-  email: "nguyenvana@example.com"
+  id: "6836d927d6b8bb6da4e00229", // ID giả lập user, ông thay bằng ID thật nếu cần
+  name: "Tran Thi B",
+  email: "anhnphe171575@fpt.edu.vn"
 };
 
 const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
--- models/blogModel.js
+++ models/blogModel.js
@@ -12,7 +12,7 @@ const blogSchema = new mongoose.Schema({
     author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User', 
-        required: true
+        
     },
     tag: {
         type: String
