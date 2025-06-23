c251a2a2996c358ef1403d8a15cc6261715c2c1f
QuanPM <anhnphe171575@fpt.edu.vn>
2025-06-24 02:55:32 +0700

    xu ly payment

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 const verifyToken = require('../middleware/auth');
-
 router.post('/create-payment', verifyToken,paymentController.createPayment);
+router.post('/callback', verifyToken, paymentController.callback);
 module.exports = router;
