c9f5b7326fb9a60a0ea09c18a488b8104f9ef652
quanpmhe171342 <quanpmhe171342@fpt.edu.vn>
2025-07-23 00:36:02 +0700

    sủa phân quyền blog

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -20,11 +20,11 @@ router.post('/', verifyToken, upload.array('images', 5), createBlog);
 
 router.get('/marketing', verifyToken,authorizeRoles(ROLES.MARKETING_MANAGER, ROLES.ORDER_MANAGER), getAllBlogs);
 
-router.get('/customer', verifyToken, getAllBlogs);
+router.get('/customer', getAllBlogs);
 
 router.get('/', getAllBlogs);
 
-router.get('/:id',verifyToken, getBlog);
+router.get('/:id', getBlog);
 
 
 router.put('/:id', verifyToken, upload.array('images', 5), updateBlog);
